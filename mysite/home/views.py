5e176d1432658967453507ba2a2c3dd3a8248d48
santoshs0612 <santoshs0612@gmail.com>
2024-12-31 16:44:17 +0530

    App Index and Contact added

---
+++ mysite/home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+
+def index(request):
+    # return HttpResponse("This is home Page")
+    return render(request,"index.html")
+
+def about(request):
+    # return HttpResponse("This is about Page")
+    return render(request,"about.html")
+
+def services(request):
+    # return HttpResponse("This is about Services")
+    return render(request,"services.html")
+
+def contact(request):
+    # return HttpResponse("This is about Contact Page")
+    return render(request,"contact.html")
