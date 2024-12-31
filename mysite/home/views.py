31ecd3ab83210b25e0e39e3c5e5af71732ac8a3c
santoshs0612 <santoshs0612@gmail.com>
2024-12-31 18:14:06 +0530

    Now Contact working fine

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponse
 from datetime import datetime
 from home.models import Contact
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -25,4 +26,5 @@ def contact(request):
         desc = request.POST.get("desc")
         contact = Contact(name=name,email = email,phone= phone, desc =desc,date = datetime.today() )
         contact.save()
+        messages.success(request, "Your Details has been Saved Thansks for visiting Santosh's IceCream!")
     return render(request,"contact.html")
